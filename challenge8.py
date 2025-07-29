44340d7c87fb600531d8018df47c5c0686620b31
Steve <stevelopez290519@gmail.com>
2025-07-29 09:46:42 -0500

    Challenge8

--- challenge8.py
+++ challenge8.py
@@ -1,5 +1,28 @@
 '''
 Dev: Steve López
 Activity: N° 8
+Description: Programa que solicita N números enteros en un vector.
+Si el nuevo valor a ingresar es igual a la SUMA del último y penúltimo valor ingresado,
+el sistema arrojará un mensaje: 
+Error: El número ingresado es igual a la suma de los dos antetiores. Intente con otro.
+Esto hasta que el número sea difernete a la SUMA mencionada. 
 '''
-import os
+numeros = []
+
+N = int(input("¿Cuantos números deseas ingresar?: "))
+
+i = 0 
+
+while i < N:
+    nuevo = int(input("Ingrese un número: "))
+    if i >= 2:
+        suma = numeros[i - 1] + numeros[i - 2]
+        if nuevo == suma:
+            print("Error: El número ingresado es igual a la suma de los dos antetiores. Intente con otro.")
+            continue
+
+    numeros.append(nuevo)
+    i = i + 1 
+
+print("Lista final de números ingresados:")
+print(numeros)   
